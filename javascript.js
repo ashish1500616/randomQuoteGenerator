a10e59fada62a1654f89efe72b0935d67acbc3cc
Ashish verma <ashish1500616@gmail.com>
2017-04-13 12:08:49 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   var quote;
   var author;
   function getQuote(){
-    var url="api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=jQuery1910567528349808494_1492065273178&_=1492065273179";
+    var url="http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=";
     $.getJSON(url, function(data){
       tweetThis = data.quoteText + " By - " + data.quoteAuthor;
       $(".quote-text").html('"'+data.quoteText+'"');
